fb6115207f04fd8c65c8c44ed046d5397291428f
Stefan <stefan.haberler@gmx.at>
2023-12-27 16:11:54 +0100

    more sandboxing; towards final db structure

--- scr/database/sb2.py
+++ scr/database/sb2.py
@@ -1,51 +1,92 @@
-from sqlalchemy import create_engine
+from pathlib import Path
+from typing import List
+import xml.etree.ElementTree as ET
 
-engine = create_engine("sqlite+pysqlite:///:memory:", echo=True)
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, Session, relationship
+from sqlalchemy import create_engine, select, ForeignKey
 
-# create a table with three columns, id, tech_id and source_type using the orm syntax
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, ForeignKey, Text
-from sqlalchemy.orm import relationship, Mapped, mapped_column
 
-Base = declarative_base()
+NAMESPACE = {'ogd': 'http://ris.bka.gv.at/ogd/V2_6'}
 
-class Document(Base):
+
+class Base(DeclarativeBase):
+    pass
+
+
+class SourceType(Base):
+    __tablename__ = "source_type"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column("name")
+    sources: Mapped[List["Document"]] = relationship(back_populates="source_type")
+    
+
+class Document(Base): 
     __tablename__ = "document"
-    
-    doc_id: Mapped[int] = mapped_column(primary_key=True)
-    source_type = mapped_column
-    tech_id = Column(String, nullable=False)
-    
-    paragraphs = relationship("Paragraph", back_populates="document")
-    
-    def __repr__(self):
-        return f"Document(doc_id={self.doc_id}, source_type={self.source_type}, tech_id={self.tech_id})"
-    
+    id: Mapped[int] = mapped_column(primary_key=True)
+    tech_id: Mapped[str] = mapped_column()
+    source_type_id: Mapped[int] = mapped_column("source_type_id", ForeignKey("source_type.id"))
+    applikation: Mapped[str] = mapped_column(nullable=True)
+    gericht: Mapped[str] = mapped_column(nullable=True)
+    geschaeftszahl: Mapped[str] = mapped_column(nullable=True)
+    entscheidungsdatum: Mapped[str] = mapped_column(nullable=True)
+    kurztitel: Mapped[str] = mapped_column(nullable=True)
+    langtitel: Mapped[str] = mapped_column(nullable=True)
+    gesetzesnummer: Mapped[int] = mapped_column(nullable=True)
+    artikelnummer: Mapped[int] = mapped_column(nullable=True)
+    paragraphennummer: Mapped[int] = mapped_column(nullable=True)
 
-class Paragraph(Base):
+    source_type: Mapped["SourceType"] = relationship(back_populates="sources")
+    paragraphs: Mapped[List["Paragraph"]] = relationship(back_populates="document")
+   
+
+
+class Paragraph(Base): 
     __tablename__ = "paragraph"
-    
-    para_id: Mapped[int] = mapped_column(primary_key=True)
-    text = Column(Text, nullable=False)
-    doc_id = Column(Integer, ForeignKey("document.doc_id"))
-    
-    document = relationship("Document", back_populates="paragraphs")
-    annotations = relationship("Annotation", back_populates="paragraph")
-    
-    def __repr__(self):
-        return f"Paragraph(para_id={self.para_id}, doc_id={self.doc_id}, text={self.text})"
+    id: Mapped[int] = mapped_column("id", primary_key=True)
+    index: Mapped[int]
+    text: Mapped[str]
+    document_id: Mapped[int] = mapped_column(ForeignKey("document.id"))
+
+    document: Mapped["Document"] = relationship(back_populates="paragraphs")
+    annotations: Mapped[List["Annotation"]] = relationship(back_populates="paragraph")
+
 
 class Annotation(Base):
     __tablename__ = "annotation"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    begin: Mapped[int]
+    end: Mapped[int]
+    label: Mapped[str]
+    version: Mapped[int]
+    paragraph_id: Mapped[int] = mapped_column(ForeignKey("paragraph.id"))
+
+    paragraph: Mapped["Paragraph"] = relationship(back_populates="annotations") 
     
-    annotation_id: Mapped[int] = mapped_column(primary_key=True)
-    para_id = Column(Integer, ForeignKey("paragraph.para_id"))
-    begin_token = Column(Integer, nullable=False)
-    end_token = Column(Integer, nullable=False)
-    label = Column(String, nullable=False)
-    version = Column(Integer, nullable=False)
-    
-    paragraph = relationship("Paragraph", back_populates="annotations")
-    
-    def __repr__(self):
-        return f"Annotation(annotation_id={self.annotation_id}, para_id={self.para_id}, begin_token={self.begin_token}, end_token={self.end_token}, label={self.label}, version={self.version})"
+
+
+if __name__ == "__main__":
+    engine = create_engine("sqlite:///test.db", echo=True)
+    # Base.metadata.create_all(engine)
+
+    meta_file = Path.cwd() / r"data\judikatur\justiz\justiz_meta_collection_all_2023.xml"
+    tree = ET.parse(meta_file)
+    root = tree.getroot()
+    xml_docs = root.findall(".//ogd:OgdDocumentReference", namespaces=NAMESPACE)
+
+    new_documents = []
+    judikatur_source = SourceType(name="judikatur")
+
+    for xml_doc in xml_docs: 
+        tech_id = xml_doc.find("ogd:Data/ogd:Metadaten/ogd:Technisch/ogd:ID", namespaces=NAMESPACE).text
+        applikation = xml_doc.find("ogd:Data/ogd:Metadaten/ogd:Technisch/ogd:Applikation", namespaces=NAMESPACE).text
+        gericht = xml_doc.find("ogd:Data/ogd:Metadaten/ogd:Judikatur/ogd:Justiz/ogd:Gericht", namespaces=NAMESPACE).text
+        geschaeftszahl = xml_doc.find("ogd:Data/ogd:Metadaten/ogd:Judikatur/ogd:Geschaeftszahl/ogd:item", namespaces=NAMESPACE).text
+        entscheidungsdatum = xml_doc.find("ogd:Data/ogd:Metadaten/ogd:Judikatur/ogd:Entscheidungsdatum", namespaces=NAMESPACE).text
+        kurztitel = None
+        langtitel = None
+        gesetzesnummer = None
+        artikelnummer = None
+        paragraphennummer = None
+        print(f"{tech_id = }\n{applikation = }\n{gericht = }\n{geschaeftszahl = }\n{entscheidungsdatum = }\n{kurztitel = }\n{langtitel = }\n{gesetzesnummer = }\n{artikelnummer = }\n{paragraphennummer = }\n")
+        break
+
